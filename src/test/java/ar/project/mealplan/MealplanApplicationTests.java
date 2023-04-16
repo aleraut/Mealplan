b81c9391a204c4fcac4211dc2224db1160e8c0c6
Aleksis Rautiainen <aleraut9@gmail.com>
2023-04-16 16:21:58 +0300

    tests2

--- src/test/java/ar/project/mealplan/MealplanApplicationTests.java
+++ src/test/java/ar/project/mealplan/MealplanApplicationTests.java
@@ -1,25 +1,13 @@
 package ar.project.mealplan;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import ar.project.mealplan.web.FoodController;
-
-@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class MealplanApplicationTests {
 
-	@Autowired
-	private FoodController foodController;
-
 	@Test
-	void contextLoads()throws Exception {
-		assertThat(foodController).isNotNull();
+	void contextLoads() {
 	}
 
 }
